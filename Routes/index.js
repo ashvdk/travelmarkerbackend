82dd17a9f7a3fd153eff2b6557baac4ded9fab7f
ashwin rao <ashwinvdrk@gmail.com>
2021-06-02 18:52:46 +0530

    add follower delete follower

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const dbconnection = require('../dbconnection');
 const verifytoken = require('../verifytoken');
+var ObjectID = require('mongodb').ObjectID;
 
 routes.get('/', (req, res) => {
   res.send('<h1>is ready</h1>');
@@ -24,22 +25,23 @@ routes.get('/getuserdetails/:uid', verifytoken, (req, res) => {
 
 routes.post('/user', verifytoken, (req, res) => {
 
-  const { uid, email, displayName, username } = req.body;
+  const { uid, email, displayName, username, photoURL } = req.body;
+  //console.log(photoURL);
   dbconnection.connect(err => {
     const collection = dbconnection.db("TravelLocationExplorer").collection("users");
-    collection.findOne({ "_id": uid }, function (err, user) {
-      if (err) console.log(err);
-      if (!user) {
-        var userObj = { _id: uid, email, displayName, username };
+    collection.find({ "username": username }).toArray(function (err, result) {
+      if (err) throw err;
+      //console.log(result);
+      if (result.length > 0) {
+        res.status(409).send({ message: "username already exists" });
+      }
+      else {
+        var userObj = { _id: uid, email, displayName, username, photoURL };
         collection.insertOne(userObj, function (err, result) {
-          if (err) console.log(err);
+          if (err) throw err;
           if (result) res.status(200).send({ message: "Successful" });
         });
       }
-      else {
-        res.status(200).send({ message: "Successful" });
-      }
-
     });
     // dbconnection.close();
   });
@@ -115,12 +117,11 @@ routes.post('/user/:uid/location', verifytoken, (req, res) => {
   });
 });
 
-routes.get('/searchusername/:username', verifytoken, (req, res) => {
-  const { username } = req.params;
-  console.log(username);
+routes.get('/searchusername', verifytoken, (req, res) => {
+  const { username, uid } = req.query;
   dbconnection.connect(async err => {
     const usercollection = dbconnection.db("TravelLocationExplorer").collection("users");
-    usercollection.find({ "username": username }).limit(5).toArray(function (err, result) {
+    usercollection.find({ "username": username, _id: { $ne: uid } }).limit(5).toArray(function (err, result) {
       if (err) throw err;
       res.status(200).send({ message: 'Successful', result });
     });
@@ -134,32 +135,70 @@ routes.post('/user/addfollowing', verifytoken, (req, res) => {
   dbconnection.connect(err => {
     const relationshipcollection = dbconnection.db("TravelLocationExplorer").collection("relationship");
     var relationshipObj = {
-      uid,
-      followerid
+      iam_uid: uid,
+      followinghim_uid: followerid
     };
     relationshipcollection.insertOne(relationshipObj, function (err, result) {
       if (err) res.status(400).send('Error');
       if (result) {
 
         console.log('inserted record', result.ops[0]);
-        res.status(200).send({ message: 'Successful', result: "Following" });
+        res.status(200).send({ message: 'Successful', result: result.ops[0]['_id'] });
       }
     });
   });
 
 });
 
+routes.get('/user/:uid/getfollowersandfollwing', verifytoken, (req, res) => {
+  const { uid } = req.params;
+  dbconnection.connect(err => {
+    const relationshipcollection = dbconnection.db("TravelLocationExplorer").collection("relationship");
+    relationshipcollection.aggregate([
+      { $match: { "followinghim_uid": uid } },
+      {
+        $lookup: {
+          from: "users",
+          localField: "iam_uid",
+          foreignField: "_id",
+          as: "users"
+        }
+      }
+    ]).toArray(function (err, results) {
+      res.status(200).send({ message: 'Successful', result: results });
+      //console.log(results);
+    })
+    // relationshipcollection.find({ $or: [{ "iam_uid": uid }, { "followinghim_uid": uid }] }).toArray(function (err, result) {
+    //   console.log(result);
+    // })
+  });
+});
+
 routes.get('/user/:uid/checkrelationship/:followerid', verifytoken, (req, res) => {
   const { uid, followerid } = req.params;
   dbconnection.connect(err => {
     const relationshipcollection = dbconnection.db("TravelLocationExplorer").collection("relationship");
-    relationshipcollection.findOne({ "uid": uid, followerid: followerid }, function (err, result) {
+    relationshipcollection.findOne({ "iam_uid": uid, followinghim_uid: followerid }, function (err, result) {
       if (result) {
         console.log(result);
-        res.status(200).send({ message: 'Successful', result: "Following" });
+        res.status(200).send({ message: 'Successful', result: result['_id'] });
       } else {
-        res.status(200).send({ message: 'Not Successful', result: "Follow" });
+        res.status(400).send({ message: 'Not Successful' });
       }
+    });
+  });
+});
+
+routes.delete('/deletefollowing', verifytoken, (req, res) => {
+  const id = req.query['_id'];
+
+  dbconnection.connect(async err => {
+    const relationshipcollection = dbconnection.db("TravelLocationExplorer").collection("relationship");
+    const _id = new ObjectID(id);
+    relationshipcollection.deleteOne({ _id: _id }, function (err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted");
+      res.status(200).send({ message: "Successful" });
     });
   });
 });
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(routes);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
 });
